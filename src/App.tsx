5a55133f70334a0d6618b2346b995bf42f1c24f5
ana.xavier <ana.xavier@m2msolutions.com.br>
2025-10-02 00:18:15 -0300

    instalando tailwind

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-3xl font-bold underline bg-cyan-400">
+
+    <h1 className="text-3xl font-bold underline bg-cyan-400">
+     travel.com
+    </h1>
+    </div>
+  )
+}
+
+export default App
